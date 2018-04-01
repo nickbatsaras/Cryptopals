f582c6d75e191052362cbc7c419922b2e2b107e3
Nick Batsaras <batsarasnikos@gmail.com>
2018-04-02 01:03:21 +0300

    Challenge 7 solved

---
+++ set1/07.py
@@ -0,0 +1,16 @@
+from cryptopals import Base64ToHex
+from Crypto.Cipher import AES
+
+KEY = b"YELLOW SUBMARINE"
+CIPHER = AES.new(KEY, AES.MODE_ECB)
+
+HEX = ""
+
+with open("07.txt") as FILE:
+    for line in FILE:
+        line = line.strip()
+        HEX += Base64ToHex(line)
+
+TEXT = CIPHER.decrypt(bytes.fromhex(HEX))
+
+print(TEXT.decode("ascii"))
